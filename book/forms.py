997eab2640b5a0dd8be1382fd3aac608d8251a83
LongYang <julyzergcn1@gmail.com>
2021-03-16 14:09:02 +0800

    use model form to valid and save data

--- book/forms.py
+++ book/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 
-from .models import Author, Publisher
+from .models import Author, Book, Publisher
 
 
-class BookForm(forms.Form):
-    name = forms.CharField()
-    price = forms.CharField()
-    author = forms.ModelMultipleChoiceField(queryset=Author.objects.all())
-    publisher = forms.ModelChoiceField(queryset=Publisher.objects.all())
+class BookForm(forms.ModelForm):
+    # name = forms.CharField()
+    # price = forms.CharField()
+    # author = forms.ModelMultipleChoiceField(queryset=Author.objects.all())
+    # publisher = forms.ModelChoiceField(queryset=Publisher.objects.all())
+
+    class Meta:
+        model = Book
+        fields = ('book_name', 'price', 'author', 'publisher')
